89e6d327841c25f07b77d5d9fb0fd6be88e06324
rishabkumar07 <rishabkumar0703gmail.com>
2020-10-08 21:08:06 +0530

    Updates

---
+++ precache-manifest.e3c8ff03a246779aa5957fcc332699e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991d3140104adefe2bdc4a706c7deb91",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "f26fb0abde78b027d7fd",
+    "url": "/ipod/static/css/main.0ba77ea8.chunk.css"
+  },
+  {
+    "revision": "f56981ce7163d01f8197",
+    "url": "/ipod/static/js/2.82643220.chunk.js"
+  },
+  {
+    "revision": "8cd96bcab000617e1a77041949003a36",
+    "url": "/ipod/static/js/2.82643220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26fb0abde78b027d7fd",
+    "url": "/ipod/static/js/main.8fb77585.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  }
+]);
